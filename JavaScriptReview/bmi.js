38f82cacfd1eddc71d08f0089a1116523a7867ce
jdalmeria-tech <alm24008@byui.edu>
2024-12-28 11:32:16 +0800

    using else and if statements

--- JavaScriptReview/bmi.js
+++ JavaScriptReview/bmi.js
@@ -1,6 +1,19 @@
 function bmiCalculator(weight, height) {
-  var bmi = weight / (Math.pow(height, 2));
-  return Math.round(bmi);
+  let bmi = weight / (Math.pow(height, 2));
+  bmi = Math.round(bmi * 10) / 10;
+  var interpretation = "";
+
+  if (bmi < 18.5) {
+    interpretation = "Your BMI is " + bmi + ", so you are underweight.";
+  }
+  if (bmi >= 18.5 && bmi <= 24.9) {
+    interpretation = "Your BMI is " + bmi + ", so you have a normal weight.";
+  }
+
+  else if (bmi >= 25) {
+    interpretation = "Your BMI is " + bmi + ", so you are overweight.";
+  }
+  return interpretation;
 }
 
 console.log(bmiCalculator(65, 1.8));
